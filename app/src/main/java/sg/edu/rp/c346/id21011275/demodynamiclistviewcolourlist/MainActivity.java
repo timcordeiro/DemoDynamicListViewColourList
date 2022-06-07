e269ce3eb3218dde4800c68039a9e201e3ecb0a6
CORDEIRO TIMOTHY ALEXANDER TAN <21011275@myrp.edu.sg>
2022-06-07 21:34:33 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21011275/demodynamiclistviewcolourlist/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.id21011275.demodynamiclistviewcolourlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etElement;
+    Button btnAdd;
+    ListView lvColour;
+
+    ArrayList<String> alColours;
+    ArrayAdapter<String> aaColour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etElement = findViewById(R.id.editTextColour);
+        btnAdd = findViewById(R.id.buttonAddItem);
+        lvColour = findViewById(R.id.listViewColour);
+
+        alColours = new ArrayList<>();
+        alColours.add("Red");
+        alColours.add("Orange");
+
+        aaColour = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alColours);
+
+        lvColour.setAdapter(aaColour);
+    }
+}
